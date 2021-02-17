3748775b279b9db12b9d1117c5536c7cdc0bb047
nining377 <nining377@gmail.com>
2021-02-17 21:44:58 +0800

    1、修复7.1.80以后脚本失效的问题

--- app/src/main/java/com/raincat/unblockmusicpro/MainActivity.java
+++ app/src/main/java/com/raincat/unblockmusicpro/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends PermissionProxyActivity {
 
         tv_version.setText(BuildConfig.VERSION_NAME);
         tv_perfect[0].setText("Google：4.3.1");
-        tv_perfect[1].setText("Global：6.0.0～7.2.0");
+        tv_perfect[1].setText("Global：6.0.0～8.1.10");
 
         share = getSharedPreferences("share", Context.MODE_WORLD_READABLE);
         originIndex = share.getInt("origin", 0);
